d8fc134c1052c9666fc163ec8bf0ec1cb060c31f
Mehdimak <m.maktbai@tu-braunschweig.de>
2020-12-07 04:03:05 -0800

    Add orthogonal matrix and Used Subplot funktion on linear algebra notebook; related to issue #7

--- docs/source/notebooks/visualization.py
+++ docs/source/notebooks/visualization.py
@@ -30,3 +30,45 @@ def plot_matrix_as_heatmap(matrix: pt.Tensor, annotate: bool=True):
     ax.set_yticklabels([])
     ax.set_xlabel("{:d} columns".format(n_cols), fontsize=LABEL_SIZE)
     ax.set_ylabel("{:d} rows".format(n_rows), fontsize=LABEL_SIZE)
+def plot_matrix_multiplication_as_heatmap(A: pt.Tensor,B: pt.Tensor,C: pt.Tensor, annotate: bool=True):
+    n_rows, n_cols = A.size()
+    n_rows, n_cols = B.size()
+    height = MATRIX_WIDTH * n_rows / n_cols
+    fig, ((ax1),(ax2),(ax3)) = plt.subplots(1, 3, figsize=(3*MATRIX_WIDTH, height))
+    ax1.imshow(A)
+    ax2.imshow(B)
+    ax3.imshow(C)
+    if annotate:
+        for row in range(n_rows):
+            for col in range(n_cols):
+                ax1.text(row, col, "{:2.2f}".format(A[col, row]),
+                        ha="center", va="center", color="w", fontsize=ANNO_SIZE)
+                ax2.text(row, col, "{:2.2f}".format(B[col, row]),
+                        ha="center", va="center", color="w", fontsize=ANNO_SIZE)
+                ax3.text(row, col, "{:2.2f}".format(C[col, row]),
+                        ha="center", va="center", color="w", fontsize=ANNO_SIZE)
+    ax1.set_xticks(pt.arange(n_cols+1) - 0.5, minor=True)
+    ax1.set_yticks(pt.arange(n_rows+1) - 0.5, minor=True)
+    ax1.grid(which="minor", color="w", linestyle='-', linewidth=1)
+    ax1.tick_params(which="minor", bottom=False, left=False)
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+    ax1.set_xlabel("{:d} columns".format(n_cols), fontsize=LABEL_SIZE)
+    ax1.set_ylabel("{:d} rows".format(n_rows), fontsize=LABEL_SIZE)
+    ax2.set_xticks(pt.arange(n_cols+1) - 0.5, minor=True)
+    ax2.set_yticks(pt.arange(n_rows+1) - 0.5, minor=True)
+    ax2.grid(which="minor", color="w", linestyle='-', linewidth=1)
+    ax2.tick_params(which="minor", bottom=False, left=False)
+    ax2.set_xticklabels([])
+    ax2.set_yticklabels([])
+    ax2.set_xlabel("{:d} columns".format(n_cols), fontsize=LABEL_SIZE)
+    ax2.set_ylabel("{:d} rows".format(n_rows), fontsize=LABEL_SIZE)
+    ax3.set_xticks(pt.arange(n_cols+1) - 0.5, minor=True)
+    ax3.set_yticks(pt.arange(n_rows+1) - 0.5, minor=True)
+    ax3.grid(which="minor", color="w", linestyle='-', linewidth=1)
+    ax3.tick_params(which="minor", bottom=False, left=False)
+    ax3.set_xticklabels([])
+    ax3.set_yticklabels([])
+    ax3.set_xlabel("{:d} columns".format(n_cols), fontsize=LABEL_SIZE)
+    ax3.set_ylabel("{:d} rows".format(n_rows), fontsize=LABEL_SIZE)
+
