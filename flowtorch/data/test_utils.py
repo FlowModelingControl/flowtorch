9b1076173d0e7ebe3c42c0641d45357b43bac100
Andre Weiner <andre.weiner@yahoo.de>
2021-07-02 20:57:20 +0200

    Add unit tests for data.utils.

--- flowtorch/data/test_utils.py
+++ flowtorch/data/test_utils.py
@@ -1,7 +1,7 @@
 # third party packages
 import pytest
 # flowtorch packages
-from flowtorch.data.utils import format_byte_size
+from flowtorch.data.utils import format_byte_size, check_and_standardize_path
 
 
 def test_byte_formatting():
@@ -11,3 +11,12 @@ def test_byte_formatting():
     assert size == 1e4/1024 and unit == "Kb"
     size, unit = format_byte_size(1e7)
     assert size == 1e7/1024**2 and unit == "Mb"
+
+
+def test_check_and_standardize_path():
+    with pytest.raises(ValueError):
+        _ = check_and_standardize_path("does/not/exist/")
+    path = check_and_standardize_path("./")
+    assert path == "."
+    path = check_and_standardize_path("./flowtorch/__init__.py", folder=False)
+    assert path == "./flowtorch/__init__.py"
