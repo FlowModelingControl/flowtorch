28b8b550457dcdf71ccd5b41dd4abd4036283b15
Andre Weiner <andre.weiner@yahoo.de>
2021-07-02 20:49:43 +0200

    Add module for global constants.

---
+++ flowtorch/constants.py
@@ -0,0 +1,8 @@
+"""Global constants for all subpackages."""
+
+from torch import float32
+
+# default data type for all PyTorch tensors
+DEFAULT_DTYPE = float32
+# default tolerance for comparison of floating point numbers
+FLOAT_TOLERANCE = 1.0e-7
