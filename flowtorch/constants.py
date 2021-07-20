c7f9dcf8e1e739303c859e746cc81ba7eba0f04e
Andre Weiner <andre.weiner@yahoo.de>
2021-07-20 11:19:56 +0200

    Remove assertion if dataset evironment variable is not defined.

--- flowtorch/constants.py
+++ flowtorch/constants.py
@@ -9,8 +9,7 @@ DEFAULT_DTYPE = float32
 # default tolerance for comparison of floating point numbers
 FLOAT_TOLERANCE = 1.0e-7
 # path to flowTorch datasets
-DATASET_PATH = environ.get("FLOWTORCH_DATASETS", "")
-assert DATASET_PATH != "", "FLOWTORCH_DATASETS environment variable not defined."
+DATASET_PATH = environ.get("FLOWTORCH_DATASETS", "./datasets/")
 # available datasets
 folders = [folder for folder in listdir(DATASET_PATH) if isdir(join(DATASET_PATH, folder))]
 folder_paths = [join(DATASET_PATH, folder) for folder in folders]
