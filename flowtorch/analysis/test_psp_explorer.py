9cab33a46dc3b0d1cf3be1103a53b3c1bad0d720
Andre Weiner <andre.weiner@yahoo.de>
2021-10-27 21:45:43 +0200

    Remove hard-coded path from PSPExplorer unittest; issue #26.

--- flowtorch/analysis/test_psp_explorer.py
+++ flowtorch/analysis/test_psp_explorer.py
@@ -3,13 +3,13 @@ import pytest
 # third party packages
 import plotly.graph_objects as go
 # flowtorch packages
+from flowtorch import DATASETS
 from .psp_explorer import PSPExplorer
 
-# this path will be replaced once non-proprietary test data is available
-PATH = "/home/andre/Downloads/input/FOR/iPSP_reference_may_2021/0226.hdf5"
 
 def test_psp_explorer():
-    explorer = PSPExplorer(PATH)
+    path = DATASETS["ipsp_fake.hdf5"]
+    explorer = PSPExplorer(path)
     times = explorer.loader.write_times
     zones = explorer.loader.zone_names
     fig = explorer.interact(zones[0], "Cp", times[:10])
