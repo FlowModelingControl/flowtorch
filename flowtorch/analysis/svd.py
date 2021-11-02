9b0b0132edcf2469f11ef405045a7ec55b351786
Andre Weiner <andre.weiner@yahoo.de>
2021-11-02 09:22:24 +0100

    Add robust SVD implementation.

--- flowtorch/analysis/svd.py
+++ flowtorch/analysis/svd.py
@@ -1,10 +1,93 @@
 """Classes and functions wrapping around *torch.linalg.svd*.
 """
 
+# standard library packages
+from math import sqrt
+from typing import Tuple, Union
 # third party packages
 import torch as pt
 # flowtorch packages
 from flowtorch.data.utils import format_byte_size
+
+
+def inexact_alm_matrix_complection(data_matrix: pt.Tensor, sparsity: float = 1.0,
+                                   tol: float = 1.0e-6, max_iter: int = 100,
+                                   verbose: bool = False) -> Tuple[pt.Tensor, pt.Tensor]:
+    """Split a data matrix in low rank and sparse contributions.
+
+    This function implements the *inexact augmented Lagrange multiplier
+    matrix completion* algorithm to solve the *principal component pursuit*
+    problem. The implementation is based on the Matlab code of Isabel Scherl
+    (link_), which is in turn based on the LRSLibrary_.
+
+    .. _link: https://github.com/ischerl/RPCA-PIV/blob/master/functions/inexact_alm_rpca.m
+    .. _LRSLibrary: https://github.com/andrewssobral/lrslibrary
+
+    :param data_matrix: input data matrix; snapshots must be
+        organized as column vectors
+    :type data_matrix: pt.Tensor
+    :param sparsity: factor to compute Lagrangian multiplyer for sparsity
+        (typically named *lambda*); lower values lead to more agressive
+        filtering
+    :type sparsity: float, optional
+    :param tol: tolerance for the normalized Frobenius norm of the difference
+        between original data and the sum of low rank and sparse contributions;
+        defaults to 1.0e-6
+    :type tol: float, optional
+    :param max_iter: maximum number of iteration before to give up, defaults to 100
+    :type max_iter: int, optional
+    :param verbose: residual is printed for every iteration if True;
+        defaults to False
+    :type verbose: bool, optional
+    :return: tuple holding the low rank and the sparse matrices
+    :rtype: Tuple[pt.Tensor, pt.Tensor]
+    """
+    row, col = data_matrix.shape
+    lambda_0 = sparsity / sqrt(row)
+    # low rank and sparse matices
+    L, S = pt.zeros_like(data_matrix), pt.zeros_like(data_matrix)
+    # matrix of Lagrange multipliers
+    Y = data_matrix.detach().clone()
+    norm_two = pt.linalg.svdvals(Y)[0].item()
+    norm_inf = pt.linalg.norm(Y, float("inf")).item()
+    dual_norm = max(norm_two, norm_inf)
+    Y /= dual_norm
+    # more hyperparameters
+    mu = 1.25 / norm_two
+    norm_data = pt.linalg.norm(data_matrix)
+    sv = 10
+    rho = 1.5
+
+    for i in range(max_iter):
+        temp = data_matrix - L + Y/mu
+        S = pt.maximum(temp - lambda_0/mu, pt.tensor(0.0))
+        S += pt.minimum(temp + lambda_0/mu, pt.tensor(0.0))
+        U, s, VH = pt.linalg.svd(data_matrix - S + Y/mu, full_matrices=False)
+        # truncate SVD
+        svp = s[s > 1.0/mu].shape[0]
+        if svp < sv:
+            sv = min(svp+1, col)
+        else:
+            sv = min(svp + round(0.05*col), col)
+        L = U[:, :svp] @ pt.diag(s[:svp] - 1.0/mu) @ VH[:svp, :]
+        # print(L[0,:])
+        # Z is the residual matrix
+        Z = data_matrix - L - S
+        # update Lagrange multipliers
+        Y += mu*Z
+        mu = min(mu*rho, mu*1.0e7)
+        # check convergence
+        residual = pt.linalg.norm(Z) / norm_data
+        if residual < tol:
+            print(f"Inexact ALM converged after {i+1} iterations")
+            print("Final residual: {:2.4e}".format(residual))
+            return L, S
+        if verbose:
+            print("Residual after iteration {:5d}: {:10.4e}".format(
+                i+1, residual.item()))
+    print(f"Inexact ALM did not converge within {max_iter} iterations")
+    print("Final residual: {:10.4e}".format(residual))
+    return L, S
 
 
 class SVD(object):
@@ -20,6 +103,14 @@ class SVD(object):
     :type s_cum: pt.Tensor
     :param V: right singular values
     :type V: pt.Tensor
+    :param L: low rank contribution to data matrix
+    :type L: pt.Tensor
+    :param S: sparse contribution to data matrix
+    :type S: pt.Tensor
+    :param robust: data_matrix is split in to low rank and sparse contributions
+        if True or if dictionary with options for Inexact ALM algorithm; the SVD
+        is computed only on the low rank matrix
+    :type robust: Union[bool,dict]
     :param rank: rank used for truncation
     :type rank: int
     :param opt_rank: optimal rank according to SVHT
@@ -46,17 +137,34 @@ class SVD(object):
     tensor([ 99.9687,  99.9996,  99.9999, 100.0000, 100.0000])
     >>> svd.U.shape
     torch.Size([400, 5])
+    >>> svd = SVD(data, rank=100, robust=True)
+    >>> svd.L.shape
+    torch.Size([400, 5])
+    >>> svd = SVD(data, rank=100, robust={"sparsity" : 1.0, "verbose" : True, "max_iter" : 100})
+    >>> svd.S.shape
+    torch.Size([400, 5])
 
     """
 
-    def __init__(self, data_matrix: pt.Tensor, rank: int = None):
+    def __init__(self, data_matrix: pt.Tensor, rank: int = None,
+                 robust: Union[bool, dict]=False):
         shape = data_matrix.shape
         assert len(shape) == 2, (
             f"The data matrix must be a 2D tensor.\
             The provided data matrix has {len(shape)} dimensions."
         )
         self._rows, self._cols = shape
-        U, s, VH = pt.linalg.svd(data_matrix, full_matrices=False)
+        self._robust = robust
+        if bool(self._robust):
+            if isinstance(robust, dict):
+                L, S = inexact_alm_matrix_complection(data_matrix, **robust)
+            else:
+                L, S = inexact_alm_matrix_complection(data_matrix)
+            self._L, self._S = L, S
+            U, s, VH = pt.linalg.svd(L, full_matrices=False)
+        else:
+            self._L, self._S = None, None
+            U, s, VH = pt.linalg.svd(data_matrix, full_matrices=False)
         self._opt_rank = self._optimal_rank(s)
         self.rank = self.opt_rank if rank is None else rank
         self._U = U[:, :self.rank]
@@ -111,13 +219,26 @@ class SVD(object):
     def s_cum(self) -> pt.Tensor:
         s_sum = self._s.sum().item()
         return pt.tensor(
-            [self._s[:i].sum().item() / s_sum * 100.0 for i in range(1, self._s.shape[0]+1)],
+            [self._s[:i].sum().item() / s_sum *
+             100.0 for i in range(1, self._s.shape[0]+1)],
             dtype=self._s.dtype
         )
 
     @property
     def V(self) -> pt.Tensor:
         return self._V
+
+    @property
+    def L(self) -> pt.Tensor:
+        return self._L
+
+    @property
+    def S(self) -> pt.Tensor:
+        return self._S
+
+    @property
+    def robust(self) -> Union[bool, dict]:
+        return self._robust
 
     @property
     def rank(self) -> int:
