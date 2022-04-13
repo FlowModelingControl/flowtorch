2848cc09a361eebce180818df4223dfac8838f73
Andre Weiner <andre.weiner@yahoo.de>
2022-04-13 11:59:13 +0200

    Extend TAUDataloader to handle distributed cases.

--- flowtorch/data/tau_dataloader.py
+++ flowtorch/data/tau_dataloader.py
@@ -10,7 +10,7 @@ to snapshot data.
 """
 # standard library packages
 from glob import glob
-from typing import List, Dict, Tuple, Union
+from typing import List, Dict, Tuple, Union, Set
 # third party packages
 from netCDF4 import Dataset
 import torch as pt
@@ -19,8 +19,11 @@ from flowtorch import DEFAULT_DTYPE
 from .dataloader import Dataloader
 from .utils import check_list_or_str, check_and_standardize_path
 
-
-IGNORE_FIELDS = ("x", "y", "z", "volume", "global_id")
+PMESH_NAME = "dualgrid_domain_{:s}_grid_1"
+PVERTEX_KEY = "pcoord"
+PWEIGHT_KEY = "pvolume"
+PADD_POINTS_KEY = "addpoint_idx"
+PLOCAL_ID_KEY = "local_id"
 VERTEX_KEYS = ("points_xc", "points_yc", "points_zc")
 WEIGHT_KEY = "volume"
 
@@ -48,7 +51,8 @@ class TAUDataloader(Dataloader):
 
     """
 
-    def __init__(self, path: str, base_name: str, dtype: str = DEFAULT_DTYPE):
+    def __init__(self, path: str, base_name: str, distributed: bool = False,
+                 dtype: str = DEFAULT_DTYPE):
         """Create loader instance from TAU simulation folder.
 
         :param path: path to TAU simulation files
@@ -56,16 +60,37 @@ class TAUDataloader(Dataloader):
         :param base_name: part of the solution file name before iteration count,
             e.g., base_name_ if the solution file is called base_name_i=0102_t=1.0
         :type base_name: str
+        :param distributed: True if mesh and solution are distributed in domain
+            files; defaults to False
+        :type distributed: bool, optional
         :param dtype: tensor type, defaults to DEFAULT_DTYPE
         :type dtype: str, optional
         """
         self._path = check_and_standardize_path(path)
         self._base_name = base_name
+        self._distributed = distributed
         self._dtype = dtype
+        if self._distributed:
+            self._domain_ids = self._get_domain_ids()
         self._time_iter = self._decompose_file_name()
+        self._mesh_data = self._load_mesh_data()
+
+    def _get_domain_ids(self) -> List[str]:
+        """Determine ids of processes domains.
+
+        Grid and solution files of distributed simulations contain a string
+        of the form *domain_id* in the filename. This function extracts the
+        *id* from the grid files.
+
+        :return: sorted list of available domain ids
+        :rtype: List[str]
+        """
+        files = glob(f"{self._path}/{PMESH_NAME.format('*')}")
+        ids = [s.split("grid_domain_")[-1].split("_")[0] for s in files]
+        return sorted(ids, key=int)
 
     def _find_grid_file(self) -> str:
-        """Determine the name of the grid file
+        """Determine the grid file's name in serial simulations.
 
         :raises FileNotFoundError: if no grid file is found
         :raises FileNotFoundError: if multiple grid files are found
@@ -89,30 +114,95 @@ class TAUDataloader(Dataloader):
             iterations as values
         :rtype: Dict[str, str]
         """
-        files = glob(f"{self._path}/{self._base_name}i=*t=*")
+        if self._distributed:
+            d0 = f".domain_{self._domain_ids[0]}"
+            files = glob(f"{self._path}/{self._base_name}i=*t=*{d0}")
+        else:
+            files = glob(f"{self._path}/{self._base_name}i=*t=*")
         if len(files) < 1:
             raise FileNotFoundError(
                 f"Could not find solution files in {self._path}/")
         time_iter = {}
+        split_at = f".domain" if self._distributed else " "
         for f in files:
-            t = f.split("t=")[-1]
+            t = f.split("t=")[-1].split(split_at)[0]
             i = f.split("i=")[-1].split("_t=")[0]
             time_iter[t] = i
         return time_iter
 
-    def _file_name(self, time: str) -> str:
+    def _file_name(self, time: str, suffix: str = "") -> str:
         """Create solution file name from write time.
 
         :param time: snapshot write time
         :type time: str
+        :param suffix: suffix to append to the file name; used for decomposed
+            simulations
+        :type suffix: str, optional
         :return: name of solution file
         :rtype: str
         """
         itr = self._time_iter[time]
-        return f"{self._path}/{self._base_name}i={itr}_t={time}"
+        return f"{self._path}/{self._base_name}i={itr}_t={time}{suffix}"
+
+    def _load_domain_mesh_data(self, pid: str) -> pt.Tensor:
+        """Load vertices and volumes for a single processor domain.
+
+        :param pid: domain id
+        :type pid: str
+        :return: tensor of size n_points x 4, where n_points is the number
+            of unique cells in the domain, and the 4 columns contain the
+            coordinates of the vertices (x, y, z) and the cell volumes
+        :rtype: pt.Tensor
+        """
+        path = f"{self._path}/{PMESH_NAME.format(pid)}"
+        with Dataset(path) as data:
+            vertices = pt.tensor(data[PVERTEX_KEY][:], dtype=self._dtype)
+            volumes = pt.tensor(data[PWEIGHT_KEY][:], dtype=self._dtype)
+            local_ids = pt.tensor(data[PLOCAL_ID_KEY][:], dtype=pt.int64)
+            n_add_points = data[PADD_POINTS_KEY].shape[0]
+
+        n_points = volumes.shape[0] - n_add_points
+        mask = pt.ones_like(volumes, dtype=pt.bool)
+        mask[local_ids >= n_points] = False
+        data = pt.zeros((n_points, 4), dtype=self._dtype)
+        data[:, 0] = pt.masked_select(vertices[:, 0], mask)
+        data[:, 1] = pt.masked_select(vertices[:, 1], mask)
+        data[:, 2] = pt.masked_select(vertices[:, 2], mask)
+        data[:, 3] = pt.masked_select(volumes, mask)
+        return data
+
+    def _load_mesh_data(self) -> pt.Tensor:
+        """Load mesh vertices and cell volumes.
+
+        :return: Tensor of dimension n_points x 4; the first three columns
+            correspond to the x/y/z coordinates, and the 4th column contains
+            the volumes
+        :rtype: pt.Tensor
+        """
+        if self._distributed:
+            return pt.cat(
+                [self._load_domain_mesh_data(pid) for pid in self._domain_ids],
+                dim=0
+            )
+        else:
+            path = f"{self._path}/{self._find_grid_file()}"
+            with Dataset(path) as data:
+                vertices = pt.stack(
+                    [pt.tensor(data[key][:], dtype=self._dtype)
+                     for key in VERTEX_KEYS],
+                    dim=-1
+                )
+                if WEIGHT_KEY in data.variables.keys():
+                    weights = pt.tensor(
+                        data.variables[WEIGHT_KEY][:], dtype=self._dtype)
+                else:
+                    print(
+                        f"Warning: could not find cell volumes in file {path}")
+                    weights = pt.ones(vertices.shape[0], dtype=self._dtype)
+            return pt.cat((vertices, weights.unsqueeze(-1)), dim=-1)
 
     def _load_single_snapshot(self, field_name: str, time: str) -> pt.Tensor:
-        """Load a single snapshot of a single field from the netCDF4 file.
+        """Load a single snapshot of a single field from the netCDF4 file(s).
 
         :param field_name: name of the field
         :type field_name: str
@@ -121,9 +211,21 @@ class TAUDataloader(Dataloader):
         :return: tensor holding the field values
         :rtype: pt.Tensor
         """
-        path = self._file_name(time)
-        with Dataset(path) as data:
-            field = pt.tensor(data.variables[field_name][:], dtype=self._dtype)
+        if self._distributed:
+            field = []
+            for pid in self._domain_ids:
+                path = self._file_name(time, f".domain_{pid}")
+                with Dataset(path) as data:
+                    field.append(
+                        pt.tensor(
+                            data.variables[field_name][:], dtype=self._dtype)
+                    )
+            return pt.cat(field, dim=0)
+        else:
+            path = self._file_name(time)
+            with Dataset(path) as data:
+                field = pt.tensor(
+                    data.variables[field_name][:], dtype=self._dtype)
         return field
 
     def load_snapshot(self, field_name: Union[List[str], str],
@@ -159,34 +261,37 @@ class TAUDataloader(Dataloader):
 
     @property
     def field_names(self) -> Dict[str, List[str]]:
+        """Find available fields in solution files.
+
+        Available fields are determined by matching the number of
+        weights with the length of datasets in the available
+        solution files; for distributed cases, the fields are only
+        determined based on *domain_0*.
+
+        :return: dictionary with time as key and list of
+            available solution fields as value
+        :rtype: Dict[str, List[str]]
+        """
         self._field_names = {}
+        if self._distributed:
+            n_points = self._load_domain_mesh_data(
+                self._domain_ids[0]).shape[0]
+            suffix = f".domain_{self._domain_ids[0]}"
+        else:
+            n_points = self.vertices.shape[0]
+            suffix = ""
         for time in self.write_times:
             self._field_names[time] = []
-            with Dataset(self._file_name(time)) as data:
-                self._field_names[time] = [
-                    key for key in data.variables.keys() if key not in IGNORE_FIELDS
-                ]
+            with Dataset(self._file_name(time, suffix)) as data:
+                for key in data.variables.keys():
+                    if data[key].shape[0] == n_points:
+                        self._field_names[time].append(key)
         return self._field_names
 
     @property
     def vertices(self) -> pt.Tensor:
-        path = f"{self._path}/{self._find_grid_file()}"
-        with Dataset(path) as data:
-            vertices = pt.stack(
-                [pt.tensor(data[key][:], dtype=self._dtype)
-                 for key in VERTEX_KEYS],
-                dim=-1
-            )
-        return vertices
+        return self._mesh_data[:, :3]
 
     @property
     def weights(self) -> pt.Tensor:
-        path = self._file_name(self.write_times[0])
-        with Dataset(path) as data:
-            if WEIGHT_KEY in data.variables.keys():
-                weights = pt.tensor(
-                    data.variables[WEIGHT_KEY][:], dtype=self._dtype)
-            else:
-                print(f"Warning: cell volumes not found in file {path}")
-                weights = None
-        return weights
+        return self._mesh_data[:, 3]
--- flowtorch/data/test_tau_dataloader.py
+++ flowtorch/data/test_tau_dataloader.py
@@ -8,6 +8,12 @@ from flowtorch.data import TAUDataloader
 
 
 class TestTAUDataloader():
+    def test_get_domain_ids(self):
+        path = DATASETS["tau_distributed"]
+        prefix = "solution_Delta_MUB_fine_iso_hybrid_58.pval.unsteady_"
+        loader = TAUDataloader(path, prefix, True)
+        assert loader._domain_ids == ["249", "258"]
+
     def test_find_grid_file(self):
         path = DATASETS["tau_backward_facing_step"]
         loader = TAUDataloader(path, "sol.pval.unsteady_")
@@ -15,29 +21,78 @@ class TestTAUDataloader():
         assert grid_file == "PW_DES-HybQuadTRex-v2_yp-50_s1.15_ny67.grd"
 
     def test_decompose_file_name(self):
+        # serial test case
         path = DATASETS["tau_backward_facing_step"]
         loader = TAUDataloader(path, "sol.pval.unsteady_")
         time_iter = loader._decompose_file_name()
         assert len(time_iter.keys()) == 2
         assert "2.9580000000e-02" in time_iter
         assert time_iter["2.9580000000e-02"] == "14790"
+        # distributed test case
+        path = DATASETS["tau_distributed"]
+        prefix = "solution_Delta_MUB_fine_iso_hybrid_58.pval.unsteady_"
+        loader = TAUDataloader(path, prefix, True)
+        time_iter = loader._decompose_file_name()
+        assert len(time_iter.keys()) == 1
+        assert "3.409440000e-01" in time_iter
+        assert time_iter["3.409440000e-01"] == "17508"
 
-    def test_properties(self):
+    def test_load_domain_mesh_data(self):
+        path = DATASETS["tau_distributed"]
+        prefix = "solution_Delta_MUB_fine_iso_hybrid_58.pval.unsteady_"
+        loader = TAUDataloader(path, prefix, True)
+        data = loader._load_domain_mesh_data("249")
+        assert data.shape == (17382, 4)
+        data = loader._load_domain_mesh_data("258")
+        assert data.shape == (17128, 4)
+
+    def test_load_mesh_data(self):
+        # serial/reconstructed
+        path = DATASETS["tau_backward_facing_step"]
+        loader = TAUDataloader(path, "sol.pval.unsteady_")
+        n_points = 1119348
+        assert loader.vertices.shape == (n_points, 3)
+        assert loader.weights.shape == (n_points,)
+        # distributed
+        path = DATASETS["tau_distributed"]
+        prefix = "solution_Delta_MUB_fine_iso_hybrid_58.pval.unsteady_"
+        loader = TAUDataloader(path, prefix, True)
+        n_points = 34510
+        assert loader.vertices.shape == (n_points, 3)
+        assert loader.weights.shape == (n_points,)
+
+    def test_write_times(self):
+        # serial/reconstructed
         path = DATASETS["tau_backward_facing_step"]
         loader = TAUDataloader(path, "sol.pval.unsteady_")
         times = loader.write_times
         assert times[0] == "2.9580000000e-02"
         assert times[-1] == "3.2190000000e-02"
-        field_names = loader.field_names
-        assert len(field_names.keys()) == 2
-        assert "density" in field_names[times[-1]]
-        n_points = 1119348
-        vertices = loader.vertices
-        assert vertices.shape == (n_points, 3)
-        weights = loader.weights
-        assert weights.shape == (n_points,)
+        # distributed
+        path = DATASETS["tau_distributed"]
+        prefix = "solution_Delta_MUB_fine_iso_hybrid_58.pval.unsteady_"
+        loader = TAUDataloader(path, prefix, True)
+        assert loader.write_times[0] == "3.409440000e-01"
+
+    def test_field_names(self):
+        # serial/reconstructed
+        path = DATASETS["tau_backward_facing_step"]
+        loader = TAUDataloader(path, "sol.pval.unsteady_")
+        times = loader.write_times
+        assert len(loader.field_names.keys()) == 2
+        assert "pressure" in loader.field_names[times[-1]]
+        assert "density" in loader.field_names[times[-1]]
+        # distributed
+        path = DATASETS["tau_distributed"]
+        prefix = "solution_Delta_MUB_fine_iso_hybrid_58.pval.unsteady_"
+        loader = TAUDataloader(path, prefix, True)
+        times = loader.write_times
+        assert len(loader.field_names.keys()) == 1
+        assert "pressure" in loader.field_names[times[0]]
+        assert "density" in loader.field_names[times[0]]
 
     def test_load_snapshot(self):
+        # serial/reconstructed
         path = DATASETS["tau_backward_facing_step"]
         loader = TAUDataloader(path, "sol.pval.unsteady_")
         times = loader.write_times
@@ -60,3 +115,25 @@ class TestTAUDataloader():
         assert f2s.shape == (n_points, 2)
         assert pt.allclose(f1s[:, -1], f1)
         assert pt.allclose(f2s[:, -1], f2)
+        # distributed
+        path = DATASETS["tau_distributed"]
+        prefix = "solution_Delta_MUB_fine_iso_hybrid_58.pval.unsteady_"
+        loader = TAUDataloader(path, prefix, True)
+        times = loader.write_times
+        n_points = 34510
+        # single snapshots, single field
+        density = loader.load_snapshot("density", times[0])
+        assert density.shape == (n_points,)
+        # multiple snapshots, single field
+        density = loader.load_snapshot("density", [times[0], times[0]])
+        assert density.shape == (n_points, 2)
+        # single snapshot, multiple fields
+        density, p = loader.load_snapshot(
+            ["density", "pressure"], times[0])
+        assert density.shape == (n_points, )
+        assert p.shape == (n_points, )
+        # multiple snapshots, multiple fields
+        density, p = loader.load_snapshot(["density", "pressure"],
+                                          [times[0], times[0]])
+        assert density.shape == (n_points, 2)
+        assert p.shape == (n_points, 2)
