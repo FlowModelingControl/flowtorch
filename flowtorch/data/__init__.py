753affc729141fa34857ce0fd9c43f88bc689641
Andre Weiner <andre.weiner@yahoo.de>
2022-09-30 09:58:30 +0200

    Add conditional import of TecplotDataloader to avoid potentially unnecessary ParaView installation.

--- flowtorch/data/__init__.py
+++ flowtorch/data/__init__.py
@@ -4,6 +4,11 @@ from .csv_dataloader import CSVDataloader
 from .vtk_dataloader import VTKDataloader
 from .psp_dataloader import PSPDataloader
 from .tau_dataloader import TAUDataloader, TAUConfig
-from .tecplot_dataloader import TecplotDataloader
 from .selection_tools import mask_box, mask_sphere
 from .outlier_tools import iqr_outlier_replacement
+try:
+    from .tecplot_dataloader import TecplotDataloader
+except Exception: 
+    print("Warning: TecplotDataloader can't be loaded. Most likely, the 'paraview' module is missing.\n" +
+    "Refer to the installation instructions at https://github.com/FlowModelingControl/flowtorch\n" +
+    "If you are not using the TecplotDataloader, ignore this warning.")
