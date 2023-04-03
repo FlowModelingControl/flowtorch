e26aba2361e537ee3e281dc6da51e8214cfa9d9b
Andre Weiner <andre.weiner@yahoo.de>
2023-04-03 21:36:01 +0200

    Add usecols option to DMD to handle non-sequential snapshots.

--- flowtorch/analysis/test_dmd.py
+++ flowtorch/analysis/test_dmd.py
@@ -2,7 +2,7 @@
 """
 
 # third party packages
-import pytest
+from pytest import raises
 import torch as pt
 # flowtorch packages
 from flowtorch import DATASETS
@@ -15,6 +15,7 @@ def test_DMD():
     loader = FOAMDataloader(path)
     times = loader.write_times[1:]
     data = loader.load_snapshot("p", times)
+    rows, cols = data.shape
     rank = 3
     dmd = DMD(data, dt=0.1, rank=rank)
     assert dmd.eigvals.shape == (rank,)
@@ -22,29 +23,29 @@ def test_DMD():
     assert dmd.eigvecs.shape == (rank, rank)
     assert dmd.eigvecs.dtype == pt.complex64
     assert dmd.modes.dtype == pt.complex64
-    assert dmd.modes.shape == (data.shape[0], rank)
+    assert dmd.modes.shape == (rows, rank)
     assert dmd.frequency.shape == (rank,)
     assert dmd.growth_rate.shape == (rank,)
     assert dmd.amplitude.shape == (rank,)
     assert dmd.amplitude.dtype == pt.complex64
-    assert dmd.dynamics.shape == (rank, data.shape[-1])
+    assert dmd.dynamics.shape == (rank, cols - 1)
     assert dmd.dynamics.dtype == pt.complex64
     assert dmd.integral_contribution.shape == (rank,)
     assert dmd.integral_contribution.dtype == pt.float32
-    assert dmd.reconstruction.shape == data.shape
+    assert dmd.reconstruction.shape == (rows, cols - 1)
     assert dmd.reconstruction.dtype == data.dtype
     partial = dmd.partial_reconstruction({0})
     assert partial.dtype == data.dtype
-    assert partial.shape == data.shape
+    assert partial.shape == (rows, cols - 1)
     partial = dmd.partial_reconstruction({0, 2})
     assert partial.dtype == data.dtype
-    assert partial.shape == data.shape
+    assert partial.shape == (rows, cols - 1)
     top = dmd.top_modes(10)
     top = dmd.top_modes(10, True)
     assert top.shape == (min(rank, 10),)
     assert top.dtype == pt.int64
-    assert dmd.reconstruction_error.shape == data.shape
-    assert dmd.projection_error.shape == (data.shape[0], data.shape[1] - 1)
+    assert dmd.reconstruction_error.shape == (rows, cols - 1)
+    assert dmd.projection_error.shape == (rows, cols - 1)
     # robust DMD
     dmd = DMD(data, dt=0.1, rank=rank, robust=True)
     assert dmd.svd.L.shape == (data.shape[0], rank+1)
@@ -68,6 +69,18 @@ def test_DMD():
     DX, DY = dmd.tlsq_error
     assert  DX.shape == (data.shape[0], data.shape[1] - 1)
     assert  DY.shape == (data.shape[0], data.shape[1] - 1)
+    # DMD with usecols parameter
+    with raises(ValueError):
+        usecols = pt.tensor([0, data.shape[1] - 1], dtype=pt.int64)
+        dmd = DMD(data, dt=0.1, usecols=usecols)
+    with raises(ValueError):
+        usecols = pt.ones(data.shape[1], dtype=pt.int64)
+        dmd = DMD(data, dt=0.1, usecols=usecols)
+    dmd = DMD(data, dt=0.1, rank=3, optimal=True, usecols=pt.tensor([0, 2, 3], dtype=pt.int64))
+    rec = dmd.reconstruction
+    assert rec.shape == (rows, 3)
+
+
 
 
 
